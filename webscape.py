b8dc027d6e9d2446b4cc7511ee53f04b25e7de07
William Chou <w.chou06@gmail.com>
2017-01-22 00:51:04 +0000

    newest webscrape.py file

--- webscape.py
+++ webscape.py
@@ -1,6 +1,3 @@
-##Abandoned this file. Was too difficult reading raw html data using the method of terminal + sublime as suggested. 
-#Instead implemented code in jupyter notbook for more clarity.
-
 from urllib import  as urlopen as UReq
 from bs4 import BeautifulSoup as soup
 my_url = "https://www.expedia.ca/Deals"
@@ -11,6 +8,9 @@ page_html = uClient.read()
 uClient.close()
 
 #next parse HTML
+page_soup=soup(page_html, "html.parser")
+
 """
 Once in beautiful soup we can traverse it.
 """
+page_soup.ffindAll(<div class="box tile-details "> <p class="secondary tile-price" id="tile-hotel-price-0-0-1"> <strong id="hotel-price-0-0-1"> C$48 </strong> <br>per night<br> <span class="cross-out-price" id="cross-out-price-0-0-1"></span> </p> <h4 class="tile-name" id="tile-hotel-name-0-0-1">Hotel only</h4> <p class="secondary">Thu, 23 Feb - Sun, 26 Feb</p> </div>)
